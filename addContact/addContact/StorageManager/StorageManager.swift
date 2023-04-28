5a9108cfd5016fc091a6b2ad55bc4c39ee6bbef6
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-04-29 02:17:19 +0300

    Creation and logic StorageManager

--- addContact/addContact/StorageManager/StorageManager.swift
+++ addContact/addContact/StorageManager/StorageManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class StorageManager {
+    static let shared = StorageManager()
+    private init() {}
+    
+    let contactsKey = "contacts"
+    let userDefaults = UserDefaults.standard
+    
+    func save(_ contact: Contact) {
+        var contacts = fetchContacts()
+        contacts.append(contact)
+        
+        guard let data = try? JSONEncoder().encode(contacts) else { return }
+        userDefaults.set(data, forKey: contactsKey)
+    }
+    
+    func fetchContacts() -> [Contact] {
+        guard let data = userDefaults.value(forKey: contactsKey) as? Data else { return [] }
+        guard let contacts = try? JSONDecoder().decode([Contact].self, from: data) else { return [] }
+        
+        return contacts
+    }
+    
+    func delete(at index: Int) {
+        var contacts = fetchContacts()
+        contacts.remove(at: index)
+        
+        guard let data = try? JSONEncoder().encode(contacts) else { return }
+        userDefaults.set(data, forKey: contactsKey)
+    }
+    
+}
