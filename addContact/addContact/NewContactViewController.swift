02c470914f8b01807a6526c5288875177b810216
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-04-28 23:37:55 +0300

    setup doneButton activity

--- addContact/addContact/NewContactViewController.swift
+++ addContact/addContact/NewContactViewController.swift
@@ -10,19 +10,29 @@ import UIKit
 class NewContactViewController: UIViewController {
 
     @IBOutlet var firstNameTextField: UITextField!
-    
     @IBOutlet var lastNameTextField: UITextField!
     
     @IBOutlet var doneButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        doneButton.isEnabled = false
+        firstNameTextField.addTarget(self, action: #selector(firstNameTextFieldDidChanged), for: .editingChanged)
     }
 
     @IBAction func cancelButtonPressed(_ sender: Any) {
+        
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Private Methods
+    @objc private func firstNameTextFieldDidChanged() {
+        if let text = firstNameTextField.text {
+            doneButton.isEnabled = !text.isEmpty ? true : false
+        }
     }
 }
 
