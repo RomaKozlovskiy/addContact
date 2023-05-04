90773435d4f09787cb06cbfbac776599bfa02215
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-05-04 12:20:22 +0300

    Work with NewContactViewController

--- addContact/addContact/ViewControllers/NewContactViewController.swift
+++ addContact/addContact/ViewControllers/NewContactViewController.swift
@@ -7,25 +7,38 @@
 
 import UIKit
 
+protocol NewContactViewControllerDelegate {
+    func saveContact(_ contact: Contact)
+}
+
 class NewContactViewController: UIViewController {
 
+    // MARK: - @IBOutlets
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
-    
     @IBOutlet var doneButton: UIBarButtonItem!
     
+    // MARK: - Public Properties
+    var delegate: NewContactViewControllerDelegate!
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        doneButton.isEnabled = false
-        firstNameTextField.addTarget(self, action: #selector(firstNameTextFieldDidChanged), for: .editingChanged)
-    }
-
-    @IBAction func cancelButtonPressed(_ sender: Any) {
         
+        doneButton.isEnabled = false
+        firstNameTextField.addTarget(
+            self,
+            action: #selector(firstNameTextFieldDidChanged),
+            for: .editingChanged
+        )
+    }
+    // MARK: - @IBActions
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
-        
+        saveAndExit()
     }
     
     // MARK: - Private Methods
@@ -33,6 +46,17 @@ class NewContactViewController: UIViewController {
         if let text = firstNameTextField.text {
             doneButton.isEnabled = !text.isEmpty ? true : false
         }
+    }
+    
+    private func saveAndExit() {
+        guard let firstName = firstNameTextField.text else { return }
+        guard let lastName = lastNameTextField.text else { return }
+        
+        let contact = Contact(firstName: firstName, lastName: lastName)
+        StorageManager.shared.save(contact: contact)
+        
+        delegate.saveContact(contact)
+        dismiss(animated: true)
     }
 }
 
